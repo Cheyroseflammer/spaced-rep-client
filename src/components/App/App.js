fb581695592500900fe3a0511a1c89ca7ca165d7
Cheyroseflammer <cheyroseflammer@gmail.com>
2020-12-29 10:28:07 -0800

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "../Header/Header";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import PublicOnlyRoute from "../PublicOnlyRoute/PublicOnlyRoute";
+import RegistrationRoute from "../../routes/RegistrationRoute/RegistrationRoute";
+import LoginRoute from "../../routes/LoginRoute/LoginRoute";
+import DashboardRoute from "../../routes/DashboardRoute/DashboardRoute";
+import LearningRoute from "../../routes/LearningRoute/LearningRoute";
+import NotFoundRoute from "../../routes/NotFoundRoute/NotFoundRoute";
+import DataContext, { LanguageProvider } from "../../contexts/DataContext";
+import "./App.css";
+
+export default class App extends Component {
+  state = { hasError: false };
+
+  static conextType = DataContext;
+
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  render() {
+    const contextValue = {
+      hasError: this.state.hasError,
+    };
+    return (
+      <LanguageProvider>
+        <div className="App">
+          <Header />
+          <main>
+            {contextValue.hasError === true ? (
+              <p>There was an error! Oh no!</p>
+            ) : null}
+            <Switch>
+              <PrivateRoute exact path={"/"} component={DashboardRoute} />
+              <PrivateRoute path={"/learn"} component={LearningRoute} />
+              <PublicOnlyRoute
+                path={"/register"}
+                component={RegistrationRoute}
+              />
+              <PublicOnlyRoute path={"/login"} component={LoginRoute} />
+              <Route component={NotFoundRoute} />
+            </Switch>
+          </main>
+        </div>
+      </LanguageProvider>
+    );
+  }
+}
---
+++ src/contexts/DataContext.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+const DataContext = React.createContext({
+  lagnuage: {},
+  words: [],
+  nextWord: "",
+  wordCorrectCount: 0,
+  wordIncorrectCount: 0,
+  totalScore: 0,
+  setDashboard: () => {},
+  setNextWord: () => {},
+});
+
+export default DataContext;
+
+export class LanguageProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lagnuage: {},
+      words: [],
+      nextWord: "",
+      wordCorrectCount: 0,
+      wordIncorrectCount: 0,
+      totalScore: 0,
+    };
+  }
+  setDashboard = (language, words) => {
+    this.setState({ language, words });
+  };
+  setNextWord = (res) => {
+    this.setState({
+      nextWord: res.nextWord,
+      wordCorrectCount: res.wordCorrectCount,
+      wordIncorrectCount: res.wordIncorrectCount,
+      totalScore: res.totalScore,
+    });
+  };
+  render() {
+    const value = {
+      language: this.state.language,
+      words: this.state.words,
+      nextWord: this.state.nextWord,
+      wordCorrectCount: this.state.wordCorrectCount,
+      wordIncorrectCount: this.state.wordIncorrectCount,
+      totalScore: this.state.totalScore,
+      setDashboard: this.setDashboard,
+      setNextWord: this.setNextWord,
+    };
+    return (
+      <DataContext.Provider value={value}>
+        {this.props.children}
+      </DataContext.Provider>
+    );
+  }
+}
